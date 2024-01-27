cf3e1e30c8510e076565b0b8020ad07d17dd4747
Josh Yim <jyim@lcs.on.ca>
2024-01-27 09:56:44 -0500

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by junxi Yim on 2024/1/27.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
